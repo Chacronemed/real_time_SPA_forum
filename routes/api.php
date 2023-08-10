0ee24d65707ba553be585155cd045a0a9ded1e1a
mohamed <entchacrone@gmail.com>
2023-08-10 14:04:19 +0100

    Likes and dislikes

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LikeController;
 
 Route::apiResource('/question', QuestionController::class);
 
 Route::apiResource('/category', CategoryController::class);
 
 Route::apiResource('/question/{question}/reply', \App\Http\Controllers\ReplyController::class);
+
+Route::post('/like/{reply}',[LikeController::class,'likeIt']);
+Route::delete('/like/{reply}',[LikeController::class,'unlikeIt']);
+
