5e94bbfd2fd3679e20d200dba44a80a8e812c75a
mohamed <entchacrone@gmail.com>
2023-08-15 20:08:16 +0100

    routes are reasyt

--- resources/js/components/AppHome.vue
+++ resources/js/components/AppHome.vue
@@ -1,6 +1,7 @@
 <template>
     <v-app>
         <toolbar></toolbar>
+        <router-view></router-view>
         <MyFooter></MyFooter>
     </v-app>
 </template>
@@ -10,10 +11,11 @@
 <script>
 import Toolbar from "./Toolbar.vue";
 import MyFooter from "./MyFooter.vue";
+import Login from "./login/login.vue";
 
 
 export default {
-    components: { Toolbar,MyFooter}
+    components: {Login, Toolbar,MyFooter}
 }
 
 </script>
